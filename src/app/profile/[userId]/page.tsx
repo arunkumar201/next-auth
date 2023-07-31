8647de5c073364b81edb15df1987c7fd451be585
arungit2020 <aruprajapati9825@gmail.com>
2023-07-31 21:58:32 +0530

    fixed linting  issue

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { useRouter } from "next/router";
 
-type UserProfileProps = {
-	params: any;
-};
-
-const UserProfile = ({ params }: UserProfileProps) => {
-	return <div className="flex h-full w-full justify-center items-center text-xl text-red-500">Profile page {params.userId}</div>;
+const UserProfile = () => {
+	return <div className="flex h-full w-full justify-center items-center text-xl text-red-500">Profile page</div>;
 };
 
 export default UserProfile;
